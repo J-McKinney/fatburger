9632fb4de1624237cb5548207f4811285e0abd2a
Jesse McKinney <jessman51386@gmail.com>
2019-09-05 15:05:50 -0400

    final copy, deploy to heroku

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+require('dotenv').config({ path: '../.env' });
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: process.env.PASSWORD,
+        database: "burgers_db"
+    });
+}
+// Make connection.
+connection.connect(err => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (`burger_name`, devoured)
+VALUES
+    ("Big Mac", 0),
+    ("Baconator", 0),
+    ("The Big King XL", 0);
+    ("The Meat Mountain Sandwich", 0);
